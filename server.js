2a30248dfef3735e4cd2e6aeb3d37e0b1c64a664
jenntress <jenntress@gmail.com>
2017-03-29 21:37:46 -0600

    added .find and .save

--- server.js
+++ server.js
@@ -3,12 +3,53 @@
 var express = require('express'); //making our application use this
 var Superhero = require('./models/superhero');
 var app     = express();
-
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 
 //app.METHOD('URL LOCATION', fucntion(req, res){})
-mongoose.connect("mongodb://locahost/superheroes");
+mongoose.connect("mongodb://127.0.0.1:27017/superheroes");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/superheroes', function(req, res){
+   Superhero.find(function(err, data){
+     if(err){
+       console.log(err);
+     } else {
+       res.json(data);
+     }
+   });
+});
+
+app.post('/superheroes', function(req, res){
+  var newSuper = new Superhero({
+    name:       req.body.name,
+    superPower: req.body.superPower,
+    universe:   req.body.universe,
+    evil:       req.body.evil,
+    rank:       req.body.rank,
+  });
+  newSuper.save(function(err, sh){
+    if(err){
+      console.log(err)
+    } else {
+      res.json(sh)
+    }
+  });
+  newSuper.save(function(err, sh){
+    if(err){
+      console.log(err)
+    } else {
+      res.json(sh)
+    }
+  });
+})
+
+app.get('/superheroes/:superhero_id', fucntion(req, res){
+  Superhero.findById(req.params.superhero_id)
+});
 
 
 
@@ -16,6 +57,11 @@ mongoose.connect("mongodb://locahost/superheroes");
 
 
 
+
+//testing to see if mongo is working
+app.post("/test", function(req, res){
+  res.send(req.body);
+});
 
 //Now we're building a little server
 var server = app.listen(3000, function(){// takes a port and a "function" to run
