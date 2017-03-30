bd04aed95efa341f7283f7ec4111de1c26dbd67f
jenntress <jenntress@gmail.com>
2017-03-29 20:52:16 -0600

    added variables for superhero properties

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 //console.log("YOU GOT IT TO WORK JENN!!"); //smoke test
 
 var express = require('express'); //making our application use this
+var Superhero = require('./models/superhero');
 var app     = express();
 
-//app.METHOD('URL LOCATION', fucntion(req, res){})
+var mongoose = require('mongoose');
 
-app.get('/test', function(req,res){
-  res.send("You found the test route!");
-}); //need to go to http://localhost:3000 on the browser to see if this works.
+
+//app.METHOD('URL LOCATION', fucntion(req, res){})
+mongoose.connect("mongodb://locahost/superheroes");
+
+
+
+
+
+
+
 
 //Now we're building a little server
 var server = app.listen(3000, function(){// takes a port and a "function" to run
